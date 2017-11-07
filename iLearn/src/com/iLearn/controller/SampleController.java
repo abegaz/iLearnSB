139c799f9bf28ccaa92c5c9a23ffe5dac790379c
Christopher Patrick (CMPATR9659) <CMPATR9659@ung.edu>
2017-11-07 15:18:30 -0500

    Added iLearn

--- iLearn/src/com/iLearn/controller/SampleController.java
+++ iLearn/src/com/iLearn/controller/SampleController.java
@@ -1,5 +1,19 @@
 package com.iLearn.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SampleController {
-	
+
+	@FXML TextField UserName;
+	@FXML TextField Password;
+	@FXML Label notice, notice2;
+
+
+
+	public void login(){
+		notice2.setText("Hello!");
+	}
 }
