62e740793d5d7d4ac7753feff7f97eb2dbbb59a6
Kyle Wilson <kyleww1996@gmail.com>
2017-11-09 20:31:56 -0500

    Added IlearnLogin

--- IlearnLogin/src/application/LoginController.java
+++ IlearnLogin/src/application/LoginController.java
@@ -16,25 +16,58 @@ public class LoginController {
 		@FXML TextField Password;
 		public void login(){
 			//DBConnect connect = new DBConnect();
-			notice.setText(UserName.getText());
-			String SQLQuery = "SELECT * FROM users WHERE id = '26'";
-			ResultSet rs = null;
+			//notice.setText(UserName.getText());
+
+			//String SQLQuery = "SELECT * FROM users LIMIT 0,10";
+			//ResultSet rs = null;
+
+			//Connection connection;
+			PreparedStatement ps;
 
 			try{
+				Connection conn = DBConnect.getConnection();
+				ps = conn.prepareStatement("SELECT `firstname`, `password` FROM `users` WHERE `firstname` =? AND `password` =?");
+				ps.setString(1, UserName.getText());
+				ps.setString(2, Password.getText());
+				ResultSet result = ps.executeQuery();
+				if(result.next()){
+					notice.setText("correct");
+				}
+				else{
+					notice.setText("no");
+				}
+
+			} catch(SQLException ex){
+				DBConnect.displayException(ex);
+			/*
+			try{
+
+
+
+				Connection connn = DBConnect.getConnection();
+				PreparedStatement statement = connn.prepareStatement("SELECT firstname FROM users LIMIT 0,10");
+				ResultSet reslut = statement.executeQuery();
+				while(reslut.next())
+				System.out.println(reslut.getString("firstname"));
+				connn.close();
+				/*
 				Connection connn = DBConnect.getConnection();
 				PreparedStatement displayprofile = connn.prepareStatement(SQLQuery);{
 					rs = displayprofile.executeQuery();
 
-					while (rs.next()){
+					while (rs.next()) {
+						//if (UserName.getText() == rs.getString("firstname"));
 						notice.setText(rs.getString("firstname"));
+
 					}
 				}
 			}
 				catch(SQLException ex){
 					DBConnect.displayException(ex);
 					}
-
+ */
 
 
 		}
+}
 }
